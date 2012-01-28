e7dd262ca6466e20452749f036dc1e1a9072aefb
Soren Hansen <soren@linux2go.dk>
2012-01-28 21:27:28 +0100

    Fix cut/paste mistake

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=get_install_requires(),
     entry_points = {
         'console_scripts': [
-            'nosetests = surveilr.admin:main',
+            'surveilr-admin = surveilr.admin:main',
         ],
     },
     test_suite='nose.collector',
